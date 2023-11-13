c4ceac0ea2089570f5090319e74e6e21c4df86a0
valerycanches <74372171+valerycanches@users.noreply.github.com>
2023-11-12 19:33:57 -0500

    proyecto inicial

---
+++ src/main/java/TDB/MSegurity/MSegurityApplication.java
@@ -0,0 +1,13 @@
+package TDB.MSegurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MSegurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MSegurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/TDB/MSegurity/MSegurityApplicationTests.java
@@ -0,0 +1,13 @@
+package TDB.MSegurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSegurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
